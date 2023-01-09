af3d75d1c25cc78dd4678eded39cc396d703b41c
HeppeDavardo <55142033+HeppeDavardo@users.noreply.github.com>
2023-01-09 16:59:48 +0100

    vue virker!

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+    <section class="vueMenu">
+      <section class="menuNav">
+      <button class="menuNavBtn"
+        v-for="tab in tabs"
+        :key="tab"
+        :class="['tab-button', { active: currentTab === tab.compName }]"
+        @click="currentTab = tab.compName">
+        {{ tab.navName }}
+      </button>
+    </section>
+      <component :is="currentTab" class="tab"></component>
+    </section>
+  </template>
+  
+  <script>
+  import Frokost from './components/Frokost.vue'
+  import IndenMaden from './components/IndenMaden.vue'
+  import SeasonMenu from './components/SeasonMenu.vue'
+  import Pizza from './components/Pizza.vue'
+  import AlaCarte from './components/AlaCarte.vue'
+  import Vin from './components/Vin.vue'
+  import Drikkevare from './components/Drikkevare.vue'
+  
+  export default {
+    components: {
+      Frokost,
+      IndenMaden,
+      SeasonMenu,
+      Pizza,
+      AlaCarte,
+      Vin,
+      Drikkevare
+    },
+    data() {
+      return {
+        currentTab: 'Frokost',
+        tabs: [
+            { navName: "Frokost", compName: "Frokost" },
+            { navName: "Inden maden", compName: "IndenMaden" },
+            { navName: "Sæson menu", compName: "SeasonMenu" },
+            { navName: "Pizza", compName: "Pizza" },
+            { navName: "A la carte", compName: "AlaCarte" },
+            { navName: "Vin", compName: "Vin" },
+            { navName: "Drikkevare", compName: "Drikkevare" }
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  @import url("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap");
+  #app {
+    font-family: "Titillium Web", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  .vueMenu {
+    display: flex;
+  }
+  .menuNav {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 20%;
+    margin-top: 5rem;
+    padding-left: 8rem;
+    border-right: 2px solid #707070;
+  }
+  .menuNavBtn{
+    background-color: transparent;
+    border: none;
+    text-align: left;
+    font-size: 20px;
+    font-weight: normal;
+    
+    cursor: pointer;
+  }
+  </style>
---
+++ src/components/Vin.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="ret"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      retter: undefined
+    };
+  },
+  created() {
+    let token;
+        // fetching private posts from category frokost
+        fetch('https://helenamarias.com/wp-json/jwt-auth/v1/token',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                 "username": "projektadmin",
+                "password": "helenajeppeadmin"
+            })
+        })
+        .then(response => response.json())
+        .then(data => { 
+            token = data.data.token;
+        })
+        .then(() => {
+            fetch('https://helenamarias.com/wp-json/wp/v2/posts?status=private&categories=10',{
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then(response => response.json())
+            .then(data => { 
+                console.log(data);
+                let ret = document.querySelector('#ret');
+
+
+                for (let i = data.length-1; i >= 0; i--) {
+                    ret.innerHTML += `
+                    <h1>${data[i].acf.kategori}</h1>
+                    <h2>${data[i].acf.navn1}</h2>
+                    <p>${data[i].acf.ingredienser1}</p>
+                    <h2>${data[i].acf.pris1}</h2>
+                    <h2>${data[i].acf.navn2}</h2>
+                    <p>${data[i].acf.ingredienser2}</p>
+                    <h2>${data[i].acf.pris2}</h2>
+                    <h2>${data[i].acf.navn3}</h2>
+                    <p>${data[i].acf.ingredienser3}</p>
+                    <h2>${data[i].acf.pris3}</h2>
+                    <h2>${data[i].acf.navn4}</h2>
+                    <p>${data[i].acf.ingredienser4}</p>
+                    <h2>${data[i].acf.pris4}</h2>
+                    <h2>${data[i].acf.navn5}</h2>
+                    <p>${data[i].acf.ingredienser5}</p>
+                    <h2>${data[i].acf.pris5}</h2>
+                    <h2>${data[i].acf.navn6}</h2>
+                    <p>${data[i].acf.ingredienser6}</p>
+                    <h2>${data[i].acf.pris6}</h2>
+                    <h2>${data[i].acf.navn7}</h2>
+                    <p>${data[i].acf.ingredienser7}</p>
+                    <h2>${data[i].acf.pris7}</h2>
+                    <h2>${data[i].acf.navn8}</h2>
+                    <p>${data[i].acf.ingredienser8}</p>
+                    <h2>${data[i].acf.pris8}</h2>
+                    `;
+                }
+            });
+        });
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
