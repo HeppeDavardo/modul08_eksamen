afa7f02bf0b79177d77c4963d476ad72cd8fda0b
HeppeDavardo <55142033+HeppeDavardo@users.noreply.github.com>
2023-01-09 17:28:12 +0100

    sidste html sider

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         {{ tab.navName }}
       </button>
     </section>
-      <component :is="currentTab" class="tab"></component>
+      <component :is="currentTab" class="tab menuTxt"></component>
     </section>
   </template>
   
@@ -51,15 +51,6 @@
   
   <style scoped>
   @import url("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap");
-  #app {
-    font-family: "Titillium Web", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  
   .vueMenu {
     display: flex;
   }
@@ -78,7 +69,10 @@
     text-align: left;
     font-size: 20px;
     font-weight: normal;
-    
     cursor: pointer;
+  }
+
+  .menuTxt {
+    
   }
   </style>
